455886a2307a786f4e32c746354e47ff9f0f41ce
Son Nguyen <dongsonktqs@gmail.com>
2018-04-11 11:05:23 +0530

    a

--- hello.py
+++ hello.py
@@ -1,17 +1,4 @@
 print("Hello")
-a = 1235345351241234123512513241234412456555555555555555555
-b = 6559856213421365412634213644123844912344921126567777777
-c = 48658979849849877817318924459844656464466456984367
-total = a + b
-diff = a - b
-product = a * b
-div = b / c
-du = b%c
-print(f"{total}")
-print(f"{diff}")
-print(f"{product}")
-print(f"{div}")
-print(f"{du}")
-#the remain of b/c
-#check b+a == a+b
-#check b/a == a/b
+# Start from 0 to 9999
+for i in range(10000):
+    print(f"happy birth day {i}")
